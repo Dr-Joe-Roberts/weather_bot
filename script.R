34dfe3d30b34ea757ed2eeb822e7c87cff7d977a
Joe Roberts <jroberts@harper-adams.ac.uk>
2021-01-17 17:53:57 +0000

    Add Twitter function to post weather update

--- script.R
+++ script.R
@@ -8,6 +8,12 @@
 # Mett Office coding definitions: https://www.metoffice.gov.uk/services/data/datapoint/uk-3-hourly-site-specific-forecast
 
 ## Setup----
+# Load packages
+library(RJSONIO)
+library(dplyr)
+library(lubridate)
+library(rtweet)
+
 # Edit .Renviron file to include API key
 # user_renviron = path.expand(file.path("~", ".Renviron"))
 # file.edit(user_renviron)
@@ -15,18 +21,9 @@
 # Call MET_API_KEY from .Renviron file
 MET_API_KEY <- Sys.getenv("MET_API_KEY")
 
-# Call Twitter credentials from .Renviron file
-CONSUMER_KEY <- Sys.getenv("TWITTER_CONSUMER_KEY")
-CONSUMER_SECRET <- Sys.getenv("TWITTER_CONSUMER_SECRET")
-ACCESS_TOKEN <- Sys.getenv("TWITTER_ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET <- Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-
 ## Weather data----
 # Define data download function
 METDataDownload <- function(stationID, product, MET_API_KEY) {
-  library(RJSONIO)
-  library(dplyr)
-  library(lubridate)
   connectStr <- paste0("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/", stationID, "?res=", product, "&key=", MET_API_KEY)
 
   con <- url(connectStr)
@@ -125,24 +122,84 @@ METDataDownload <- function(stationID, product, MET_API_KEY) {
 }
 
 # Download data
-data <- METDataDownload(stationID = 352811, product = "daily", MET_API_KEY)
+raw_data <- METDataDownload(stationID = 352811, product = "daily", MET_API_KEY)
 
-# Subset data for current day
-weather_data <- data[[3]] %>%
-  # Slice out first row
-  slice(1) %>%
-  # Remove columns containing night data
-  select(-Gm, -Hm, -PPn, -Nm, -Type, -FNm) %>%
-  # Rename columns
-  rename(
-    wind_direction = D,
-    wind_gust = Gn,
-    relative_humidity = Hn,
-    precipitation_probability = PPd,
-    wind_speed = S,
-    visibility = V,
-    max_temp = Dm,
-    feels_like_temp = FDm,
-    weather_type = W,
-    uv_index = U
+## Twitter post----
+# Define tweet function
+tweetDailyWeather <- function() {
+  
+  # Subset data for current day
+  weather_data <- raw_data[[3]] %>%
+    # Slice out first row
+    slice(1) %>%
+    # Remove columns containing night data
+    select(-Gm, -Hm, -PPn, -Nm, -Type, -FNm) %>%
+    # Rename columns
+    rename(
+      wind_direction = D,
+      wind_gust = Gn,
+      relative_humidity = Hn,
+      precipitation_probability = PPd,
+      wind_speed = S,
+      visibility = V,
+      max_temp = Dm,
+      feels_like_temp = FDm,
+      weather_type = W,
+      uv_index = U,
+      date = Date
+    )
+  
+  # Convert weather type into 'real world' values
+  weather_type <- weather_data$weather_type
+  if (weather_type == "NA") {
+    weather_type <- "not available" } else if (weather_type == "0") {
+      weather_type <- "clear" } else if (weather_type == "1") {
+        weather_type <- "sunny" } else if (weather_type == "3") {
+            weather_type <- "partly cloudy" } else if (weather_type == "5") {
+              weather_type <- "misty" } else if (weather_type == "6") {
+                weather_type <- "foggy" } else if (weather_type == "7") {
+                  weather_type <- "cloudy" } else if (weather_type == "8") {
+                    weather_type <- "overcast" } else if (weather_type == "10") {
+                      weather_type <- "light rain showers" } else if (weather_type == "11") {
+                        weather_type <- "drizzle" } else if (weather_type == "12") {
+                          weather_type <- "light rain" } else if (weather_type == "14") {
+                            weather_type <- "heavy rain showers" } else if (weather_type == "15") {
+                              weather_type <- "heavy rain" } else if (weather_type == "17") {
+                                weather_type <- "sleet showers" } else if (weather_type == "18") {
+                                  weather_type <- "sleet" } else if (weather_type == "20") {
+                                    weather_type <- "hail showers" } else if (weather_type == "21") {
+                                      weather_type <- "hail" } else if (weather_type == "23") {
+                                        weather_type <- "light snow showers" } else if (weather_type == "24") {
+                                          weather_type <- "light snow" } else if (weather_type == "26") {
+                                            weather_type <- "heavy snow showers" } else if (weather_type == "27") {
+                                              weather_type <- "heavy snow" } else if (weather_type == "29") {
+                                                weather_type <- "thunder showers" } else if (weather_type == "30") {
+                                                  weather_type <- "thunder" }
+  
+  # Create rtweet token using credentials in .Renviron file
+  token <- create_token(
+    app = "HarperAdamsWeatherBot",
+    consumer_key = Sys.getenv("TWITTER_CONSUMER_KEY"),
+    consumer_secret = Sys.getenv("TWITTER_CONSUMER_SECRET"),
+    access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+    access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+    set_renv = FALSE
   )
+  
+  # Compose tweet
+  post_tweet(status = paste0("The weather forecast for today is ", 
+                             weather_type, 
+                             " with highs of ", 
+                             weather_data$max_temp, 
+                             " °C and a ",
+                             weather_data$precipitation_probability,
+                             " % chance of precipitation.",
+                             "\n",
+                             "\n",
+                             "Updated on ", 
+                             weather_data$date,
+                             sep = ""), 
+             token = token)
+  }
+
+tweetDailyWeather()
