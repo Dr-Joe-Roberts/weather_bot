a5138ab0b8e100d5c3c5ba8645b2559a646b1dda
Joe Roberts <joe.mark.roberts@gmail.com>
2022-03-12 10:09:21 +0000

    Add pollen script

---
+++ AccuWeather.R
@@ -0,0 +1,54 @@
+## HEADER----
+## Project: Twitter weather bot [R]
+## Last modified: 22022-03-11
+## Last modified by: Joe Roberts
+
+
+## Background----
+# Met Office data function: https://www.r-bloggers.com/2019/02/weather-forecast-from-met-office/
+# Met Office coding definitions: https://www.metoffice.gov.uk/services/data/datapoint/uk-3-hourly-site-specific-forecast
+# Pollen: https://github.com/JakeRuss/sneezr and https://www.accuweather.com/ 
+
+
+## Setup----
+# Load packages
+  library(RJSONIO)
+  library(plyr)
+  library(dplyr)
+  library(lubridate)
+  library(emoji)
+  library(rtweet)
+
+# Edit .Renviron file to include API key
+# user_renviron = path.expand(file.path("~", ".Renviron"))
+# file.edit(user_renviron)
+
+# Call ACCU_WEATHER_API_KEY from .Renviron file
+ACCU_WEATHER_API_KEY <- Sys.getenv("ACCU_WEATHER_API_KEY")
+
+
+## Weather data----
+# Define data download function
+.DataDownload <- function(location, ACCU_WEATHER_API_KEY) {
+  connectStr <- paste0("http://dataservice.accuweather.com/forecasts/v1/daily/1day/", 
+                       location, 
+                       "?apikey=", 
+                       ACCU_WEATHER_API_KEY,
+                       "%20&details=true&metric=true")
+  
+  con <- url(connectStr)
+  data.json <- fromJSON(paste(readLines(con), collapse = ""))
+  close(con)
+  
+  # Specify data to extract from json file
+  grass_pollen <- data.json[["DailyForecasts"]][[1]][["AirAndPollen"]][[2]]
+  weed_pollen <- data.json[["DailyForecasts"]][[1]][["AirAndPollen"]][[4]]
+  tree_pollen <- data.json[["DailyForecasts"]][[1]][["AirAndPollen"]][[5]]
+  
+  pollen <- as_tibble(rbind(grass_pollen, weed_pollen, tree_pollen))
+  
+}
+
+data <-.DataDownload(location = 2521389, ACCU_WEATHER_API_KEY)
+
+
--- script.R
+++ script.R
@@ -29,11 +29,11 @@ MET_API_KEY <- Sys.getenv("MET_API_KEY")
 # Define data download function
 .METDataDownload <- function(stationID, product, MET_API_KEY) {
   connectStr <- paste0("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/", stationID, "?res=", product, "&key=", MET_API_KEY)
-
+  
   con <- url(connectStr)
   data.json <- fromJSON(paste(readLines(con), collapse = ""))
   close(con)
-
+  
   # Station
   LocID <- data.json$SiteRep$DV$Location$`i`
   LocName <- data.json$SiteRep$DV$Location$name
@@ -41,7 +41,7 @@ MET_API_KEY <- Sys.getenv("MET_API_KEY")
   Lat <- data.json$SiteRep$DV$Location$lat
   Lon <- data.json$SiteRep$DV$Location$lon
   Elev <- data.json$SiteRep$DV$Location$elevation
-
+  
   Details <- data.frame(
     LocationID = LocID,
     LocationName = LocName,
@@ -52,7 +52,7 @@ MET_API_KEY <- Sys.getenv("MET_API_KEY")
   )
   # Parameters
   param <- do.call("rbind", data.json$SiteRep$Wx$Param)
-
+  
   # Forecast
   if (product == "daily") {
     dates <- unlist(lapply(data.json$SiteRep$DV$Location$Period, function(x) {
@@ -66,7 +66,7 @@ MET_API_KEY <- Sys.getenv("MET_API_KEY")
     }))
     colnames(DayForecast)[ncol(DayForecast)] <- "Type"
     colnames(NightForecast)[ncol(NightForecast)] <- "Type"
-
+    
     ForecastDF <- plyr::rbind.fill.matrix(DayForecast, NightForecast) %>%
       as_tibble() %>%
       mutate(Date = as.Date(rep(dates, 2))) %>%
@@ -95,16 +95,16 @@ MET_API_KEY <- Sys.getenv("MET_API_KEY")
       do.call("rbind", x)
     }))
     colnames(Forecast)[ncol(Forecast)] <- "Hour"
-
+    
     DateTimes <- seq(ymd_hms(paste0(as.Date(dates[1]), " 00:00:00")), ymd_hms(paste0(as.Date(dates[length(dates)]), " 21:00:00")), "3 hours")
-
+    
     if (nrow(Forecast) < length(DateTimes)) {
       extra_lines <- length(DateTimes) - nrow(Forecast)
       for (i in 1:extra_lines) {
         Forecast <- rbind(rep("0", ncol(Forecast)), Forecast)
       }
     }
-
+    
     ForecastDF <- Forecast %>%
       as_tibble() %>%
       mutate(Hour = DateTimes) %>%
@@ -120,8 +120,8 @@ MET_API_KEY <- Sys.getenv("MET_API_KEY")
         W = as.numeric(W)
       )
   }
-
-
+  
+  
   list(Details, param, ForecastDF)
 }
 
@@ -160,29 +160,29 @@ raw_data <- .METDataDownload(stationID = 352811, product = "daily", MET_API_KEY)
     weather_type <- "not available" } else if (weather_type == "0") {
       weather_type <- "clear" } else if (weather_type == "1") {
         weather_type <- "sunny" } else if (weather_type == "3") {
-            weather_type <- "partly cloudy" } else if (weather_type == "5") {
-              weather_type <- "misty" } else if (weather_type == "6") {
-                weather_type <- "foggy" } else if (weather_type == "7") {
-                  weather_type <- "cloudy" } else if (weather_type == "8") {
-                    weather_type <- "overcast" } else if (weather_type == "10") {
-                      weather_type <- "light rain showers" } else if (weather_type == "11") {
-                        weather_type <- "drizzle" } else if (weather_type == "12") {
-                          weather_type <- "light rain" } else if (weather_type == "14") {
-                            weather_type <- "heavy rain showers" } else if (weather_type == "15") {
-                              weather_type <- "heavy rain" } else if (weather_type == "17") {
-                                weather_type <- "sleet showers" } else if (weather_type == "18") {
-                                  weather_type <- "sleet" } else if (weather_type == "20") {
-                                    weather_type <- "hail showers" } else if (weather_type == "21") {
-                                      weather_type <- "hail" } else if (weather_type == "23") {
-                                        weather_type <- "light snow showers" } else if (weather_type == "24") {
-                                          weather_type <- "light snow" } else if (weather_type == "26") {
-                                            weather_type <- "heavy snow showers" } else if (weather_type == "27") {
-                                              weather_type <- "heavy snow" } else if (weather_type == "29") {
-                                                weather_type <- "thunder showers" } else if (weather_type == "30") {
-                                                  weather_type <- "thunder" }
+          weather_type <- "partly cloudy" } else if (weather_type == "5") {
+            weather_type <- "misty" } else if (weather_type == "6") {
+              weather_type <- "foggy" } else if (weather_type == "7") {
+                weather_type <- "cloudy" } else if (weather_type == "8") {
+                  weather_type <- "overcast" } else if (weather_type == "10") {
+                    weather_type <- "light rain showers" } else if (weather_type == "11") {
+                      weather_type <- "drizzle" } else if (weather_type == "12") {
+                        weather_type <- "light rain" } else if (weather_type == "14") {
+                          weather_type <- "heavy rain showers" } else if (weather_type == "15") {
+                            weather_type <- "heavy rain" } else if (weather_type == "17") {
+                              weather_type <- "sleet showers" } else if (weather_type == "18") {
+                                weather_type <- "sleet" } else if (weather_type == "20") {
+                                  weather_type <- "hail showers" } else if (weather_type == "21") {
+                                    weather_type <- "hail" } else if (weather_type == "23") {
+                                      weather_type <- "light snow showers" } else if (weather_type == "24") {
+                                        weather_type <- "light snow" } else if (weather_type == "26") {
+                                          weather_type <- "heavy snow showers" } else if (weather_type == "27") {
+                                            weather_type <- "heavy snow" } else if (weather_type == "29") {
+                                              weather_type <- "thunder showers" } else if (weather_type == "30") {
+                                                weather_type <- "thunder" }
   
   # Set weather emoji
- weather_emoji <- weather_type
+  weather_emoji <- weather_type
   if (weather_emoji == "NA") {
     weather_emoji == " " } else if (weather_emoji == "clear") {
       weather_emoji <- emoji("sun") } else if (weather_emoji == "sunny") {
@@ -207,7 +207,7 @@ raw_data <- .METDataDownload(stationID = 352811, product = "daily", MET_API_KEY)
                                             weather_emoji <- emoji("cloud_with_snow") } else if (weather_emoji == "thunder showers") {
                                               weather_emoji <- emoji("cloud_with_lightning_and_rain") } else if (weather_emoji == "thunder") {
                                                 weather_emoji <- emoji("high_voltage") }
-
+  
   # Create rtweet token using credentials in .Renviron file
   token <- create_token(
     app = "HarperAdamsWeatherBot",
@@ -229,6 +229,6 @@ raw_data <- .METDataDownload(stationID = 352811, product = "daily", MET_API_KEY)
                              weather_emoji,
                              sep = ""), 
              token = token)
-  }
+}
 
 .tweetDailyWeather()
